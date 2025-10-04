f9172669c44293adc84e6a911393cb7af2ca46e3
RistoFlink <kenssi@gmail.com>
2025-10-04 20:31:32 +0300

    Describe ray-sphere intersection

--- sphere.h
+++ sphere.h
@@ -6,42 +6,46 @@
 #include "hittable.h"
 
 class sphere : public hittable {
-    public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+public:
+  sphere(const point3 &center, double radius)
+      : center(center), radius(std::fmax(0, radius)) {
+    // TODO: initialize the material pointer mat
+  }
+  bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
+    vec3 oc = center - r.origin();
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius * radius;
 
-        bool hit (const ray& r, interval ray_t, hit_record& rec) const override {
-            vec3 oc = center - r.origin();
-            auto a = r.direction().length_squared();
-            auto h = dot(r.direction(), oc);
-            auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h * h - a * c;
+    if (discriminant < 0) {
+      return false;
+    }
 
-            auto discriminant = h*h - a*c;
-            if (discriminant < 0) {
-                return false;
-            }
+    auto sqrtd = std::sqrt(discriminant);
 
-            auto sqrtd = std::sqrt(discriminant);
+    // Find the nearest root that lies in the acceptable range
+    auto root = (h - sqrtd) / a;
+    if (!ray_t.surrounds(root)) {
+      root = (h + sqrtd) / a;
+      if (!ray_t.surrounds(root)) {
+        return false;
+      }
+    }
 
-            // Find the nearest root that lies in the acceptable range
-            auto root = (h - sqrtd) / a;
-            if (!ray_t.surrounds(root)) {
-                root = (h + sqrtd) / a;
-                if (!ray_t.surrounds(root)) {
-                    return false;
-                }
-            }
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
+    rec.mat = mat;
 
-            rec.t = root;
-            rec.p = r.at(rec.t);
-            vec3 outward_normal = (rec.p - center) / radius;
-            rec.set_face_normal(r, outward_normal);
+    return true;
+  }
 
-            return true;
-        }
-
-    private:
-        point3 center;
-        double radius;
+private:
+  point3 center;
+  double radius;
+  shared_ptr<material> mat;
 };
 
 #endif
