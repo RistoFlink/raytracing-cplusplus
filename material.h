812ab9d9486712a16af430504b6d7e550ae1b7e6
RistoFlink <kenssi@gmail.com>
2025-10-04 20:55:14 +0300

    Model light scatter and reflectance

--- material.h
+++ material.h
@@ -2,6 +2,7 @@
 #define MATERIAL_H
 
 #include "hittable.h"
+#include "vec3.h"
 
 class material {
 public:
@@ -13,4 +14,24 @@ public:
   }
 };
 
+class lambertian : public material {
+public:
+  lambertian(const color &albedo) : albedo(albedo) {}
+
+  bool scatter(const ray &r_in, const hit_record &rec, color &attenuation,
+               ray &scattered) const override {
+    auto scatter_direction = rec.normal + random_unit_vector();
+
+    // catch degenerate scatter direction
+    if (scatter_direction.near_zero())
+      scatter_direction = rec.normal;
+
+    scattered = ray(rec.p, scatter_direction);
+    attenuation = albedo;
+    return true;
+  }
+
+private:
+  color albedo;
+};
 #endif // !MATERIAL_H
