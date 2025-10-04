89f96991a80458f5adb3b3d4c8000f23acf54b4b
RistoFlink <kenssi@gmail.com>
2025-10-04 19:20:19 +0300

    Implement gamma correction

--- color.h
+++ color.h
@@ -6,19 +6,31 @@
 
 using color = vec3;
 
-void write_color(std::ostream& out, const color& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+inline double linear_to_gamma(double linear_component) {
+  if (linear_component > 0)
+    return std::sqrt(linear_component);
 
-    // Translate the [0,1] component values to the byte range [0,255]
-    static const interval intensity(0.000, 0.999);
-    int rbyte = int(256 * intensity.clamp(r));
-    int gbyte = int(256 * intensity.clamp(g));
-    int bbyte = int(256 * intensity.clamp(b));
+  return 0;
+}
 
-    // Write the translated values to the output stream
-    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+void write_color(std::ostream &out, const color &pixel_color) {
+  auto r = pixel_color.x();
+  auto g = pixel_color.y();
+  auto b = pixel_color.z();
+
+  // Apply a linear to gamme transform for gamma 2
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
+
+  // Translate the [0,1] component values to the byte range [0,255]
+  static const interval intensity(0.000, 0.999);
+  int rbyte = int(256 * intensity.clamp(r));
+  int gbyte = int(256 * intensity.clamp(g));
+  int bbyte = int(256 * intensity.clamp(b));
+
+  // Write the translated values to the output stream
+  out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 
 #endif
